5b7f1d4290db92bc894f405cb26a4012e1366e39
Akash Jaiswal <akashjaiswal3846@gmail.com.com>
2021-06-06 15:44:07 +0530

    First commit

---
+++ game.js
@@ -0,0 +1,10 @@
+var helloFrom = function (personName) {
+    return "Hello from " + personName;
+}
+
+var people = ['Tom', 'Yoda', 'Ron'];
+
+people.push('Bob');
+people.push('Dr Evil');
+
+people.pop();
